9b162b4d5cd8c92ce4857c4fcc8803091c82b6c7
조대범 <38426670+DBrider3@users.noreply.github.com>
2024-02-22 19:21:28 +0900

    [level 0] Title: 모음 제거, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120849. 모음 제거/모음 제거.py
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.py
@@ -1,8 +1,5 @@
 def solution(my_string):
-    string_list = list(my_string)
-    mo = ["a","e","i","o","u"]
-    for i, value in enumerate(string_list):
-        if value in mo:
-            string_list[i] = ''
-    # my_string.replace()
-    return "".join(string_list)
+    vowels = ["a","e","i","o","u"]
+    for vowel in vowels:
+        my_string = my_string.replace(vowel, '')
+    return my_string
