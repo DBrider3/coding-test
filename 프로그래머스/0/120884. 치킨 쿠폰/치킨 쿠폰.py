fa137dc822da8e2321266aabde52de567eb079a9
조대범 <38426670+DBrider3@users.noreply.github.com>
2024-02-22 15:55:19 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.py
@@ -0,0 +1,10 @@
+def solution(chicken):
+
+    answer = 0
+    coupon = 0
+    while chicken:
+        coupon += chicken
+        chicken = coupon // 10
+        coupon %= 10
+        answer += chicken
+    return answer
