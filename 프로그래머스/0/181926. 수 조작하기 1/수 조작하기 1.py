c7305e91e981745f2b10df794f997c5881d860db
조대범 <38426670+DBrider3@users.noreply.github.com>
2024-02-27 21:01:55 +0900

    [level 0] Title: 수 조작하기 1, Time: 2.61 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.py
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.py
@@ -1,6 +1,3 @@
-from functools import reduce
 def solution(n, control):
     dic = {"w": 1, "s": -1, "d": 10, "a": -10}
-    for key in control:
-        n += dic[key]
-    return n
+    return n + sum(dic[key] for key in control)
