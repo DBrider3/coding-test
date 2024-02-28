f85b0c72aa4aa173360b5bc9461a5b2cd4b576e0
조대범 <38426670+DBrider3@users.noreply.github.com>
2024-02-28 14:30:34 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.02 ms, Memory: 9.95 MB -BaekjoonHub

--- 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.py
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.py
@@ -1,2 +1,5 @@
 def solution(my_string):
-    return ''.join([s.lower() if s.isupper() else s.upper() for s in my_string])
+    # version 1
+    # return ''.join([s.lower() if s.isupper() else s.upper() for s in my_string])
+    # version 2
+    return my_string.swapcase()
