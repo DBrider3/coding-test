008bada4e9dc1bf239968c436515cdd1989f8617
조대범 <38426670+DBrider3@users.noreply.github.com>
2024-02-28 19:11:28 +0900

    [level 0] Title: 문자열 바꿔서 찾기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.py
+++ 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.py
@@ -1,8 +1,3 @@
 def solution(m, p):
-    m = list(m)
-    for i, v in enumerate(m):
-        if v == 'A':
-            m[i] = 'B'
-        elif v == 'B':
-            m[i] = 'A'
-    return 1 if p in ''.join(m) else 0
+    return 1 if p in ''.join(['B' if i == 'A' else 'A' for i in m]) else 0
+    
