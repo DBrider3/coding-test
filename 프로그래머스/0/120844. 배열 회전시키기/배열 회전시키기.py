748785a4812e4d07860a49e96dbc2c74b3edc013
조대범 <38426670+DBrider3@users.noreply.github.com>
2024-03-05 14:46:20 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.py
+++ 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.py
@@ -1,11 +1,3 @@
 def solution(numbers, direction):
-    answer = []
-    if direction == "right":
-        answer.append(numbers[-1])
-        for i in range(len(numbers) - 1):
-            answer.append(numbers[i])
-    elif direction == "left":
-        for i in range(1, len(numbers)):
-            answer.append(numbers[i])
-        answer.append(numbers[0])  
-    return answer
+    return [numbers[-1]] + numbers[:-1] if direction == "right" else numbers[1:] + [numbers[0]]
+    
