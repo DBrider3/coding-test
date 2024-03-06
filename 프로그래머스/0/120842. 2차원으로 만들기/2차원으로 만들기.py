f93847cc652319f00085f79d09d5d87bd27aefb7
조대범 <38426670+DBrider3@users.noreply.github.com>
2024-03-06 18:29:17 +0900

    [level 0] Title: 2차원으로 만들기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120842. 2차원으로 만들기/2차원으로 만들기.py
@@ -0,0 +1,10 @@
+def solution(num_list, n):
+    answer = []
+
+    tmp = []
+    for num in num_list:
+        tmp.append(num)
+        if len(tmp) == n:
+            answer.append(tmp)
+            tmp = []
+    return answer
