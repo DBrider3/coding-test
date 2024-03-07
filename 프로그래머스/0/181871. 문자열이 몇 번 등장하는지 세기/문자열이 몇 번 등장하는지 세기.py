03fa0329b2dd9f78999515574d40d88081ee866d
조대범 <38426670+DBrider3@users.noreply.github.com>
2024-03-07 13:39:03 +0900

    [level 0] Title: 문자열이 몇 번 등장하는지 세기, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181871. 문자열이 몇 번 등장하는지 세기/문자열이 몇 번 등장하는지 세기.py
@@ -0,0 +1,7 @@
+def solution(myString, pat):
+    answer = 0
+    for i in range(len(myString)):
+        if pat in myString[i:i + len(pat)]:
+            answer += 1
+    print(myString.count(pat))
+    return answer
