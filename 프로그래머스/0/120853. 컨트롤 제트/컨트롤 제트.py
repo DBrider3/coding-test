3fa403af044f1280e70ab3c73db44d24c64081da
조대범 <38426670+DBrider3@users.noreply.github.com>
2024-03-15 14:27:05 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.py
+++ 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.py
@@ -1,19 +1,9 @@
-def is_digit(s):
-    try:
-        tmp = int(s)
-        return True
-    except ValueError:
-        return False
-
 def solution(s):
-    answer = 0
-    splitS = s.split(' ')
-    tmp = 0
-    for i in splitS:
-        if is_digit(i):
-            tmp = int(i)
-            answer += tmp
+    answer = []
+    for i in s.split():
+        if i == 'Z':
+            if answer:
+                answer.pop()
         else:
-            answer -= tmp
-        
-    return answer
+            answer.append(int(i))
+    return sum(answer)
